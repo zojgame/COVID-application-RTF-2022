591f3d5a04186e077fc91e9246153598540b6c22
RenatZubakin <zubakinr03@mail.ru>
2022-04-08 19:14:40 +0500

    first commit navigation menu

--- App.js
+++ App.js
@@ -1,28 +1,10 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
-import { StatusBar } from 'expo-status-bar';
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'oldlace',
-    justifyContent: 'center',
-    borderBottomColor: 'black',
-    borderBottomWidth: 2,
-  },
-  fontText: {
-    marginTop: 10,
-    marginBottom: 10,
-    marginLeft: 10,
-    alignItems: 'center',
-    borderBottomWidth: 2,
-    textAlign: 'center',
-    fontSize: 25,
-  },
-});
+import React from 'react';
+import Navigation from "./src/Navigation/navigation";
+
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.fontText}>ПРИЛОЖЕНИЕ</Text>
-    </View>
+      <Navigation/>
   );
 }
