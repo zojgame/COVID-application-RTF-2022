125c8de49a32e90fd662c4cdf1d6def6af73e7a6
RenatZubakin <zubakinr03@mail.ru>
2022-04-30 04:47:54 +0500

    update homepage

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import {View, Text, ScrollView, Image, StyleSheet, ImageBackground, Dimensions} from 'react-native';
 import { deviceHeight, deviceWidth } from "../const";
+import {BackgroundImage} from "react-native-elements/dist/config";
 
 const styles=StyleSheet.create({
     image1:{
-        height: deviceHeight*35/100,
-        width:'100%',
+        maxWidth:'100%',
+        minHeight:deviceHeight*35/100,
     },
     container:{
         display:'flex',
@@ -16,78 +17,100 @@ const styles=StyleSheet.create({
         fontSize:20,
         marginLeft:deviceHeight*3/100,
         marginRight:deviceHeight*3/100,
-        textAlign:"center",
-        // border:'3px solid rgb(121, 206, 231)',
+        textAlign:'center',
+        border:'3px solid rgba(2, 82, 166,0.3)',
         borderRadius:20,
         backgroundColor:'rgba(89, 134, 247,0.6);'
     },
     description:{
-        marginTop:deviceHeight*5/100,
         display:'flex',
-        flexDirection:'column',
-        justifyContent:"center",
-        alignItems:'center',
-        marginLeft:15,
-        marginRight:15,
+        flexDirection:'row',
+        width:'100%',
+        margin:deviceHeight*3/100,
+        justifyContent:'space-around',
     },
-    descriptionText1:{
-        color:'white',
-        fontSize:25,
-        width:deviceWidth*70/100,
-        // borderBottom:'2px solid rgb(121, 206, 231)',
-        borderRadius:15,
-        alignSelf:'center',
+    descriptionContainer:{
+      display:"flex",
+
+    },
+    descriptionHeader:{
+        fontSize:12,
         textAlign:'center',
-        paddingLeft:10,
-        paddingRight:10,
-        backgroundColor:'rgba(89, 134, 247,0.6)',
-        marginBottom: 10,
-        marginTop: 10
+        color:'white',
+        border:'3px solid rgba(2, 82, 166,0.3)',
+        borderRadius:10,
+        backgroundColor:'rgb(150, 177, 210)'
 
     },
     descriptionText:{
-        marginBottom: 10,
-        fontSize:20,
-        width:deviceWidth*90/100,
-        alignSelf:'center',
+        fontSize:10,
         textAlign:'center',
-        marginTop:deviceHeight*1/100,
-        color:'rgba(95, 145, 240,1);',
-        borderRadius:5,
-        borderLeftWidth: 10,
-        borderLeftColor:'rgb(150, 177, 245)',
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 5,
-        },
-        shadowOpacity: 0.1,
-        shadowRadius: 1.00,
-        elevation: 10,
-
+        marginBottom:deviceHeight * 1 / 100,
+        borderBottomWidth:1,
+        color:'#00253a',
     },
     image2:{
-        width:deviceWidth*50/100,
-            height: deviceHeight*25/100,
-    }
+        width:'30%',
+        height:'100%',
 
+    },
+    immunityContainer:{
+        display:'flex',
+        width:'100%',
+        flexDirection:'column',
+        alignItems:'center',
+    },
+    immunityHeader:{
+        color:'white',
+        fontSize:15,
+        border:'2px solid rgba(2, 82, 166,0.3)',
+        borderRadius:10,
+        backgroundColor:'rgb(150, 177, 220)',
+        marginBottom:deviceHeight * 2 / 100,
+    },
+    immunityTextC:{
+        display:'grid',
+        gridTemplateColumns: '40% 40%',
+        justifyContent:'space-between',
+    },
+    immunityText:{
+        textAlign:'center',
+        fontSize:15,
+        marginBottom:4,
+        border:'none',
+        backgroundColor:' rgba(2, 82, 166,0.1)',
+        borderRadius:10,
+
+    },
 })
 
 function HomePage(props) {
   return (
     <ScrollView style={styles.container}>
-        <Image style={styles.image1} source={require('../Images/homepage3.webp')}/>
+        <Image  style={styles.image1}  source={require('../Images/homepage3.webp')}/>
         <Text style={styles.text}>{"Данное приложение поможет вам реабилитироваться после перенесенной болезни Covid-19."}</Text>
         <View style={styles.description}>
-            <Image style={styles.image2} source={require('../Images/homepage4.png')}/>
-            <View>
-                <Text style={styles.descriptionText1}> Здесь вы узнаете:</Text>
-                <Text style={styles.descriptionText}>Как Covid-19 влияет на организм</Text>
-                <Text style={styles.descriptionText}>Время на восстановление</Text>
-                <Text style={styles.descriptionText}>Восстановление дыхательных органов</Text>
-                <Text style={styles.descriptionText}>Восстановление глотания и голоса</Text>
-                <Text style={styles.descriptionText}>Восстановление работы сердца</Text>
+            <View style={styles.descriptionContainer}>
+                <Text style={styles.descriptionHeader}>На данной странице вы узнаете:</Text>
+                <Text style={styles.descriptionText}>Как covid-19 влияет на организм</Text>
+                <Text style={styles.descriptionText}>Воможные осложнения после болезни</Text>
+                <Text style={styles.descriptionText}>Время восстановления после болезни</Text>
+                <Text style={styles.descriptionText}>Рекомендации по восстановлению</Text>
             </View>
+            <Image style={styles.image2} resizeMode={'contain'} source={require('../Images/homepage4.png')}/>
+        </View>
+        <View style={styles.immunityContainer}>
+            <Text style={styles.immunityHeader}>Укрепление иммунитета после Covid-19: </Text>
+            <BackgroundImage resizeMode={'center'} source={require('../Images/homePage5.png')}>
+                <View style={styles.immunityTextC}>
+                    <Text style={styles.immunityText}>Вести активный отдых</Text>
+                    <Text style={styles.immunityText}>Избегать стресса</Text>
+                    <Text style={styles.immunityText}>Соблюдать правила личной гигиены</Text>
+                    <Text style={styles.immunityText}>Отказаться от алкогольных напитков</Text>
+                    <Text style={styles.immunityText}>Следить за своим питанием</Text>
+                    <Text style={styles.immunityText}>Спать достаточно времени</Text>
+                </View>
+            </BackgroundImage>
         </View>
     </ScrollView>
   );
