68913953a71de3968aeee95c88c3bc2c1add126a
RenatZubakin <zubakinr03@mail.ru>
2022-05-03 04:34:24 +0500

    add on homePage covidApi and update

---
+++ src/Pages/HomePage/HomePageConstructor.js
@@ -0,0 +1,28 @@
+import React, {Component, useEffect, useState} from "react";
+import HomePage from "./HomePage";
+
+export default function HomePageConstructor(props){
+    let [sickData,changeData]=useState([])
+    useEffect(()=>{
+        covid().then(data=>changeData(data));
+    },[])
+console.log(sickData)
+    return(
+        <HomePage newSick={sickData.NewCases} newDeaths={sickData.NewDeaths} />
+    )
+}
+
+async function covid(){
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Host': 'vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com',
+            'X-RapidAPI-Key': 'b6dc7f20c1msh782f7e53afa0c87p1049b2jsna6fb61add859'
+        }
+    };
+    const data=await fetch('https://vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com/api/npm-covid-data/europe', options)
+        .then(data=>data.json())
+        .then(data=>data[3])
+
+    return data;
+}
---
+++ src/Pages/HomePage/HomePageStyles.js
@@ -0,0 +1,135 @@
+import {StyleSheet} from "react-native";
+import React from "react";
+
+export default function HomePageStyles() {
+    const styles =StyleSheet.create({
+        container: {
+            backgroundColor: '#8D74C8',
+            display: 'flex',
+            flexDirection: 'column',
+        },
+        block1: {
+            width: '100%',
+            display: 'flex',
+        },
+        backImage: {
+            width: '100%',
+            height: '55vw',
+        },
+        headerTxtBlock: {
+            marginTop: '33%',
+        },
+        headerTxt: {
+            textAlign: 'center',
+            color: 'white',
+            backgroundColor: 'rgba(141, 116, 200,0.3);',
+            fontSize: '10vw',
+        },
+        imagesBlock: {
+            backgroundColor: 'white',
+            borderTopLeftRadius: 20,
+            borderTopRightRadius: 20,
+            paddingTop: '5vw',
+            paddingBottom: '7vw',
+            marginBottom: '1vw',
+            position: 'relative',
+            display: 'grid',
+            top: '-10%',
+            gridTemplateColumns: '20% 20% 20% 20%',
+            justifyContent: 'space-around',
+        },
+        img: {
+            width: '80%',
+            height: '20vw',
+            margin: 'auto',
+        },
+        linksBlock: {
+            marginTop: 0,
+            height: 200,
+            backgroundColor: 'white',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-evenly'
+
+        },
+        linksBlockTitle: {
+            color: 'black',
+            fontSize: '7vw',
+        },
+        links: {
+            display: 'flex',
+            flexDirection: 'row',
+            overflow: 'hidden',
+            overflowX: 'scroll',
+            whiteSpace: 'nowrap',
+        },
+        linksItem: {
+            margin: 2,
+            padding: '3vw',
+            width: '37vw',
+            height: '37vw',
+            borderRadius: '10%',
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between'
+        },
+        linksTxt: {
+            color: 'white',
+            fontSize: '4vw',
+        },
+        linksImage: {
+            width: '15vw',
+            height: '15vw',
+        },
+        containterStat: {
+            display: 'flex',
+            flexDirection: 'row',
+            marginTop: '10vw',
+            marginBottom: '10vw',
+            width: '90vw',
+            margin: 'auto',
+            border: '1vw solid white',
+            borderRadius: '3vw',
+            backgroundColor: 'rgba(255,255,255,0.3)'
+
+        },
+        imgStatBlock: {
+            height: '20vw',
+            borderRadius: '10%',
+        },
+        imgStat: {
+            width: '20vw',
+            height: '20vw',
+            backgroundColor: 'white',
+            borderRadius: '10%',
+        },
+
+        contentStatBlock: {
+            display: 'flex',
+            width: '60vw',
+            flexDirection: 'column',
+            justifyContent: 'space-evenly',
+            marginRight: 'auto',
+            marginLeft: 'auto',
+        },
+        contentStatBlockHeader: {
+            textAlign: 'center',
+            fontSize: '5vw',
+            color: 'white',
+        },
+        contentStat: {
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+        },
+        contentStatText: {
+            color: 'white',
+            fontSize: '3.5vw',
+            textAlign: 'center'
+        }
+    })
+
+    return(styles)
+}
+
+
